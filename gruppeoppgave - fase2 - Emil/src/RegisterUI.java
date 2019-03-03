842e850283fd706ae67cfda206051766b9ae7de2
perkynades <45180092+perkynades@users.noreply.github.com>
2019-03-03 22:51:31 +0100

    Basic cleanup

--- gruppeoppgave - fase2 - Emil/src/RegisterUI.java
+++ gruppeoppgave - fase2 - Emil/src/RegisterUI.java
@@ -1,10 +1,16 @@
-import java.sql.SQLOutput;
 
 /**
  * This class of my newspaper register with UI!
  *
  * This class handles all the UI text which will
  * be displayed to the user.
+ *
+ * In this class you can:
+ *  create a text based UI for a simple newspaper register
+ *  search for newspapers by a specific title
+ *  list all the newspaper in our newspaper register
+ *  start our RegisterUI
+ *  stop our RegisterUI
  *
  * @author Emil Elton Nilsen
  * @version 27.02.2019
@@ -20,6 +26,13 @@ public class RegisterUI {
         parser = new Parser();
     }
 
+    /**
+     * The method that handles how we start our Register UI,
+     * and how we stop our RegisterUI.
+     *
+     * To start the application this method must be called
+     * in main.
+     */
     public void start() {
         printWelcome();
 
@@ -31,6 +44,9 @@ public class RegisterUI {
         System.out.println("Thank you for using our store. Have a good day!");
     }
 
+    /**
+     *
+     */
     private void printWelcome() {
         System.out.println("Welcome!");
         System.out.println("This is my new super awesome newspaper store!");
@@ -38,6 +54,12 @@ public class RegisterUI {
         System.out.println();
     }
 
+    /**
+     * This method processes the user input commands.
+     *
+     * @param command the command the a user types into the UI
+     * @return wantToExit, that checks if 'exit' has been typed in the UI
+     */
     private boolean processCommand (Command command) {
         boolean wantToExit = false;
 
@@ -61,6 +83,9 @@ public class RegisterUI {
         return wantToExit;
     }
 
+    /**
+     * This method checks if our user has entered the 'exit' command
+     */
     private boolean exit(Command command) {
         if (command.hasSecondWord()) {
             System.out.println("Exit what?");
@@ -71,11 +96,15 @@ public class RegisterUI {
         }
     }
 
+    /**
+     * A method tha prints out a help message, which contains all available commands
+     */
     private void printHelp() {
         System.out.println("Im sorry that you are having trouble using the store,");
         System.out.println("Maby some of these command words can help you? ");
         parser.showCommands();
     }
+
     /**
      * Prints out a full list of all the newspapers in our list of Newspapers with
      * all the information about our newspapers.
@@ -86,19 +115,30 @@ public class RegisterUI {
 
     }
 
+    /**
+     * Lets the user search for a newspaper just by entering the title.
+     * If there is a newspaper connected to the title, the UI will list all
+     * necessary information about the newspaper.
+     *
+     * If there is no newspaper connected to the title, a error message
+     * will be printed in the UI
+     *
+     * @param command 'search' + the title of the desired newspaper
+     */
     public void printNewspaperByTitle(Command command) {
         if (!command.hasSecondWord()) {
             //if there is no second word, we dont know what to search for
             System.out.println("After 'search' type in the newspaper you want to search for");
         }
 
-
         String searchWord = command.getSecondWord();
         if (newspaperRegister.findNewspaperByTitle(searchWord) == null) {
             System.out.println("The newspaper you searched for is not in our store...");
         }
         else {
-            System.out.println(newspaperRegister.findNewspaperByTitle(searchWord));
+            Newspaper newspaper = newspaperRegister.findNewspaperByTitle(searchWord);
+            System.out.println(newspaper.getTitle() + ", " + newspaper.getPublisher() + ", " + newspaper.getPublishedDate() + ", " +
+                               newspaper.getPrice() + newspaper.getCurrency() + "\n");
         }
     }
 
