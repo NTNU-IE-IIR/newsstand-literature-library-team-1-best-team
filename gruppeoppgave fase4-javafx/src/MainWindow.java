8ff6ee3e86a3ce87ce17c51c27e2e73b076eb8a1
Emil <emil.nilsen9888@gmail.com>
2019-04-18 10:29:58 +0200

    Start of phase 4 with GUI

--- gruppeoppgave fase4-javafx/src/MainWindow.java
+++ gruppeoppgave fase4-javafx/src/MainWindow.java
@@ -1,19 +1,89 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
 
+    private TextHandler textHandler;
+
+    public MainWindow() {
+        textHandler = new TextHandler();
+    }
+
+
     @Override
-    public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 500, 500);
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane root = new BorderPane();
+        VBox topContainer = new VBox();
+        MenuBar mainMenu = createMenues();
+        createWelcomeText();
+
+        topContainer.getChildren().add(mainMenu);
+
+        root.setTop(topContainer);
+        root.setCenter(createWelcomeText());
+
+        Scene scene = new Scene(root, 500, 450);
+
+        primaryStage.setTitle("Elton & Schei Kiosk");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private MenuBar createMenues() {
+        MenuBar menuBar = new MenuBar();
+
+        Menu menuFile = new Menu("File");
+        MenuItem openFile = new MenuItem("Open");
+        MenuItem printFile = new MenuItem("Print");
+        menuFile.getItems().addAll(openFile, printFile);
+
+        Menu menuEdit = new Menu("Edit");
+
+        Menu menuView = new Menu("View");
+
+        Menu menuList = new Menu("List");
+        MenuItem listAll = new MenuItem("All");
+        MenuItem listByType = new MenuItem("By type");
+        MenuItem listByPublisher = new MenuItem("By publisher");
+        menuList.getItems().addAll(listAll, listByType, listByPublisher);
+
+        Menu menuSearch = new Menu("Search");
+        MenuItem searchTitle = new MenuItem("Title");
+        MenuItem searchPublisher = new MenuItem("Publisher");
+        menuSearch.getItems().addAll(searchTitle, searchPublisher);
+
+        Menu menuConvert = new Menu("Convert");
+        MenuItem bookToSeries = new MenuItem("Book to book-series");
+        menuConvert.getItems().add(bookToSeries);
+
+        Menu menuAdd = new Menu("Add");
+
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView, menuList, menuSearch, menuConvert, menuAdd);
+
+        return menuBar;
+    }
+
+    private GridPane createWelcomeText() {
+        GridPane welcomeText = new GridPane();
+        Text text = textHandler.welcomeText();
+
+        welcomeText.setAlignment(Pos.CENTER);
+        welcomeText.getChildren().add(text);
+
+        return welcomeText;
     }
 }
---
+++ gruppeoppgave fase4-javafx/src/TextHandler.java
@@ -0,0 +1,12 @@
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class TextHandler {
+
+    public Text welcomeText() {
+        Text text = new Text("Welcome to the store!");
+        text.setFont(Font.font("Sans-serif", FontWeight.NORMAL, 30));
+        return text;
+    }
+}
